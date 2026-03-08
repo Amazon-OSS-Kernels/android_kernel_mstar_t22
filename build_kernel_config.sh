928bd0a6b3decfbe890ab2094e52cb564b4ffa86
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 08:44:56 -0500

    JVC_FireTVEditionUK-7.6.9.0-20250314

--- build_kernel_config.sh
+++ build_kernel_config.sh
@@ -2,18 +2,16 @@
 #
 #  build_kernel_config.sh
 #
-#  Copyright (c) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  Copyright (c) 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 ################################################################################
 
-export BUILD_KERNLE_LOCAL_MK=true
-KERNEL_SUBPATH="kernel/mstar/t31/4.9"
-DEFCONFIG_NAME="t31_user_defconfig"
+KERNEL_SUBPATH="kernel/mstar/t22/4.9"
+DEFCONFIG_NAME="t22_defconfig"
 TARGET_ARCH="arm64"
 TOOLCHAIN_REPO="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
-TOOLCHAIN_NAME="aarch64-linux-android-4.9"
 TOOLCHAIN_PREFIX="aarch64-linux-android-"
-MAKE_DTBS=
+MAKE_DTBS=y
 
 # Expected image files are seperated with ":"
 KERNEL_IMAGES="arch/arm64/boot/Image:arch/arm64/boot/Image.gz"
