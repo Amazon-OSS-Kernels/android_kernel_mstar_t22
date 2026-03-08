0274af64e82854f0903cf548ef3afb3bf3eaaa85
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 06:54:22 -0500

    Grundig_Vision_6_FireTVEdition-7.6.1.4-20230414 [bootable]

--- bootable/bootloader/uboot-mstar/t22/u-boot-2011.06/MstarApp/src/MsUtility.c
+++ bootable/bootloader/uboot-mstar/t22/u-boot-2011.06/MstarApp/src/MsUtility.c
@@ -1182,16 +1182,11 @@ int is_lockdown()
 int chk_cmd_lockdown(const char* command)
 {
    if ( (is_lockdown()) &&
-        ( (strstr(command, ";")) ||  /* only accept single command in a line */
-          (strstr(command, "$")) ||  /* no $ is allowed */
-          ( (strncmp("fastboot", command, strlen("fastboot"))) &&
-            (strncmp("reset", command, strlen("reset"))) &&
-            (strncmp(command, "usb", strlen("usb")) || strncmp("usb", command, strlen(command))) &&
-            (strncmp("estar", command, strlen("estar"))) &&
-            (strncmp("onetimeunlock", command, strlen("onetimeunlock")))
-          )
-        )
-      )
+        (strncmp("fastboot", command, strlen("fastboot"))) &&
+        (strncmp("reset", command, strlen("reset"))) &&
+        (strncmp("usb", command, strlen("usb"))) &&
+        (strncmp("estar", command, strlen("estar"))) &&
+        (strncmp("onetimeunlock", command, strlen("onetimeunlock"))) )
       return 0;
    else
       return 1;
