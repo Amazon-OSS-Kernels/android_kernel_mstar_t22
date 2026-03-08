f85a2ff915080a3c8eb2477e19c2a42c9e743458
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 08:23:41 -0500

    Base release import: JVC_FireTVEditionUK-7.6.2.4-20230225

---
+++ build_kernel_config.sh
@@ -0,0 +1,17 @@
+################################################################################
+#
+#  build_kernel_config.sh
+#
+#  Copyright (c) 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+################################################################################
+
+KERNEL_SUBPATH="kernel/mstar/t22/4.9"
+DEFCONFIG_NAME="t22_defconfig"
+TARGET_ARCH="arm64"
+TOOLCHAIN_REPO="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
+TOOLCHAIN_PREFIX="aarch64-linux-android-"
+MAKE_DTBS=y
+
+# Expected image files are seperated with ":"
+KERNEL_IMAGES="arch/arm64/boot/Image:arch/arm64/boot/Image.gz"
