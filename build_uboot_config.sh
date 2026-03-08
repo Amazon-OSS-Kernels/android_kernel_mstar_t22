40feafde97035c0ccd284523f7689745542e21a7
deadman96385 <seanhoyt963@gmail.com>
2026-03-08 08:25:10 -0500

    JVC_FireTVEditionUK-7.6.1.4-20230414

--- build_uboot_config.sh
+++ build_uboot_config.sh
@@ -2,14 +2,14 @@
 #
 #  build_uboot_config.sh
 #
-#  Copyright (c) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  Copyright (c) 2019-2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 ################################################################################
 
-UBOOT_SUBPATH="bootable/bootloader/uboot-mstar/t31/sboot"
+UBOOT_SUBPATH="bootable/bootloader/uboot-mstar/t22/sboot"
 
 # Expected image files are seperated with ":"
-UBOOT_IMAGES="out/unsigned/chunk_header.bin:out/unsigned/hash0.bin:out/unsigned/sboot.bin.unsigned:out/unsigned/u-boot.bin"
+UBOOT_IMAGES="out/unsigned/chunk_header.bin.unsigned:out/unsigned/hash0.bin:out/unsigned/sboot.bin.unsigned:out/unsigned/u-boot.bin"
 
 ################################################################################
 # NOTE: You must fill in the following with the path to a copy of an
